4a979cf6233f3645c6872379813ff568c0ca6670
Apiuww <alifrhasya200@gmail.com>
2026-02-18 01:49:51 +0700

    Delete pkl directory

--- pkl/hapus_perusahaan.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION['user_id'])){ header("Location: index.php"); exit(); }
-require 'koneksi.php';
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM perusahaan WHERE id='$id'");
-header("Location: perusahaan.php");
-?>
